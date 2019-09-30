314611faa3bc39fc0900d3e15df4b19e93e8ef4b
joseluisng17 <joselunajarg@gmail.com>
2019-09-30 10:44:29 -0500

    primer commit

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,46 +1,98 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import './styles/BadgesList.css';
+import Gravatar from './Gravatar';
 
 class BadgesListItem extends React.Component {
-  render() {
-    return (
-      <div className="BadgesListItem">
-        <img
-          className="BadgesListItem__avatar"
-          src={this.props.badge.avatarUrl}
-          alt={`${this.props.badge.firstName} ${this.props.badge.lastName}`}
-        />
+    render() {
+        return ( 
+            <div className = "BadgesListItem" >
+                <Gravatar className = "BadgesListItem__avatar"
+                email = { this.props.badge.email }
+                alt = "Avatar" />
 
-        <div>
-          <strong>
-            {this.props.badge.firstName} {this.props.badge.lastName}
-          </strong>
-          <br />@{this.props.badge.twitter}
-          <br />
-          {this.props.badge.jobTitle}
-        </div>
-      </div>
-    );
-  }
+                <div >
+                    <strong > { this.props.badge.firstName } { this.props.badge.lastName } </strong> <br /> @ { this.props.badge.twitter }
+                    <br /> { this.props.badge.jobTitle } 
+                </div>  
+            </div>
+        );
+    }
 }
 
-class BadgesList extends React.Component {
-  render() {
-    return (
-      <div className="BadgesList">
-        <ul className="list-unstyled">
-          {this.props.badges.map(badge => {
-            return (
-              <li key={badge.id}>
-                <BadgesListItem badge={badge} />
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
+//Custom Hooks
+function useSearchBadges(badges){
+
+    const [query, setQuery] = React.useState('');
+    const [filteredBadges, setFilteredBadges] = React.useState(badges);
+
+    React.useMemo(() => {    
+
+        const result = badges.filter(badge =>{
+            return `${badge.firstName} ${badge.lastName}`
+                .toLowerCase()
+                .includes(query.toLowerCase());
+        });
+
+        setFilteredBadges(result)
+    }, [badges, query]);
+
+    return {query, setQuery, filteredBadges}
+
+}
+
+function BadgesList (props) {
+    const badges = props.badges;
+
+    const { query, setQuery, filteredBadges} = useSearchBadges(badges);
+
+        if (filteredBadges.length === 0) {
+            return ( 
+                <div >
+                    <div className="form-group">
+                        <label>Filter badges</label>
+                        <input type="text" className="form-control"
+                            value={query}
+                            onChange={(e) => {
+                                setQuery(e.target.value);
+                            }}
+                        />
+                    </div>
+                    
+                    <h3 > No badges were found </h3> 
+                    <link className = "btn btn-primary" to = "/badges/new" > 
+                        Create new badge 
+                    </link> 
+                </div >
+            )
+        }
+
+        return ( 
+            <div className = "BadgesList" >
+                <div className="form-group">
+                    <label>Filter Bagdes</label>
+                    <input type="text" className="form-control"
+                        value={query}
+                        onChange={(e) => {
+                            setQuery(e.target.value);
+                        }}
+                    />
+                </div>
+                <ul className = "list-unstyled" > 
+                    {filteredBadges.map(badge => {
+                        return (
+                            <li key = { badge.id } >
+                                <Link className="text-reset text-decoration-none" to={`/badges/${badge.id}/edit`}>
+                                    <BadgesListItem badge = { badge }/> 
+                                </Link>    
+                            </li>
+                        );
+                    })
+                } 
+                </ul> 
+            </div >
+        );
+    
 }
 
 export default BadgesList;
